7dd756474d76bc0f8fcda53de9b988974c714bdd
HusainRF <justgetupanddoit9@gmail.com>
2023-10-23 13:26:18 +0530

    first commit

---
+++ src/screens/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../component/Navbar'
+import Footer from '../component/Footer'
+
+export default function Home() {
+  return (
+    <>
+        <div> <Navbar/> </div>
+        <div>Body</div>
+        <div> <Footer/> </div>
+    </>
+  )
+}
